938d30f99c65fe416a2abc5e7d1cdf400a00ad6e
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-10-10 19:54:55 -0500

    update

--- src/pages/BackOffice/components/Dashboard/Dashboard.js
+++ src/pages/BackOffice/components/Dashboard/Dashboard.js
@@ -1,101 +1,97 @@
 import React from "react";
-import { Row, Col, Button} from 'reactstrap';
+import { Row, Col, Button } from "reactstrap";
 
-import classnames from 'classnames';
+import classnames from "classnames";
 import Pack from "../Pack";
 import Widget from "../Widget";
-import Header from './Header';
+import Header from "./Header";
 
 import s from "./Dashboard.module.scss";
 
-
 let pack = [
-    {
-        "partner": "12",
-        "value": "100",
-        "status" : "enable",
-        "branch" : {
-          "left" : {
-            "status" : "enable",
-            "value": "200"
-          },
-          "center" : {
-            "status" : "disabled",
-            "value": "200"
-          },
-          "right" : {
-            "status" : "disabled",
-            "value": "200"
-          }
-        },
-        "name": "100"
-    },{
-        "partner": "12",
-        "value": "200",
-        "status" : "enable",        
-        "branch" : {
-          "left" : {
-            "status" : "enable",
-            "value": "200"
-          },
-          "center" : {
-            "status" : "enable",
-            "value": "200"
-          },
-          "right" : {
-            "status" : "disabled",
-            "value": "200"
-          }
-        },
-        "name": "200"
-    },{
-        "partner": "12",
-        "value": "400",
-        "status" : "enable",
-        "branch" : {
-          "left" : {
-            "status" : "disabled",
-            "value": "200"
-          },
-          "center" : {
-            "status" : "disabled",
-            "value": "200"
-          },
-          "right" : {
-            "status" : "disabled",
-            "value": "200"
-          }
-        },
-        "name": "400"
-    },{
-        "partner": "12",
-        "value": "800",
-        "status" : "buy",
-        "name": "800"
-    }, {
-        "partner": "12",
-        "value": "100",
-        "status" : "disabled",
-        "name": "100"
-    },{
-        "partner": "12",
-        "value": "200",
-        "status" :"disabled",
-        "name": "200"
-    },{
-        "partner": "12",
-        "value": "400",
-        "status" : "disabled",
-        "name": "400"
-    },{
-        "partner": "12",
-        "value": "800",
-        "status" : "disabled",
-        "name": "800"
-    }
+  {
+    partner: "12",
+    value: "100",
+    status: "enable",
+    branch: {
+      left: {
+        status: "enable"
+      },
+      center: {
+        status: "disabled"
+      },
+      right: {
+        status: "disabled"
+      },
+    },
+    name: "100",
+  },
+  {
+    partner: "12",
+    value: "200",
+    status: "enable",
+    branch: {
+      left: {
+        status: "enable"
+      },
+      center: {
+        status: "enable"
+      },
+      right: {
+        status: "disabled"
+      },
+    },
+    name: "200",
+  },
+  {
+    partner: "12",
+    value: "400",
+    status: "enable",
+    branch: {
+      left: {
+        status: "disabled"
+      },
+      center: {
+        status: "disabled"
+      },
+      right: {
+        status: "disabled"
+      },
+    },
+    name: "400",
+  },
+  {
+    partner: "12",
+    value: "800",
+    status: "disabled",
+    name: "800 TRX",
+  },
+  {
+    partner: "12",
+    value: "100",
+    status: "disabled",
+    name: "1600 TRX",
+  },
+  {
+    partner: "12",
+    value: "200",
+    status: "disabled",
+    name: "3200 TRX",
+  },
+  {
+    partner: "12",
+    value: "400",
+    status: "disabled",
+    name: "400 TRX",
+  },
+  {
+    partner: "12",
+    value: "800",
+    status: "disabled",
+    name: "800 TRX",
+  },
 ];
 class Dashboard extends React.Component {
-  
   constructor(props) {
     super(props);
     this.state = {
@@ -106,39 +102,42 @@ class Dashboard extends React.Component {
 
   render() {
     return (
-      <div className={s.root}>        
-        <Widget title={<Header/>} collapse close="">
-        <Row>
-        {
-          pack.map((item, index) => (      
-            <Col lg={2} xl={3} xs={12}>
-              <Pack index={++index} {...item}/>
-            </Col>
-          ))
-        }
-        </Row>
-        <footer className={[s.cardFooter, 'text-sm', 'card-footer'].join(' ')} style={{height: '50px',maxHeight: '50px'}}>
-        <Row className="mt">
-                  <Col md={3} xs={12}>
-                  <div
-           color="transparent"
-            className="btn-xs float-left py-0"
-            id="load-notifications-btn"  style={{height: '45px',maxHeight: '45px'}}
+      <div className={s.root}>
+        <Widget title={<Header />} collapse close="">
+          <Row>
+            {pack.map((item, index) => (
+              <Col lg={2} xl={3} xs={12}>
+                <Pack index={++index} {...item} />
+              </Col>
+            ))}
+          </Row>
+          <footer
+            className={[s.cardFooter, "text-sm", "card-footer"].join(" ")}
+            style={{ height: "50px", maxHeight: "50px" }}
           >
-            <i className="fa fa-refresh" /> Recycle count
-                      </div>
-                    </Col>
-                    <Col md={3} xs={12}>
-                    <div
-             color="transparent"
-             className="btn-xs float-left py-0"
-            id="load-notifications-btn"   style={{height: '45px',maxHeight: '45px'}}
-          >
-            <i className="fa fa-users" /> Number partners in the slot
-                      </div>
-                    </Col>
-       </Row>
-        </footer>
+            <Row className="mt">
+              <Col md={3} xs={12}>
+                <div
+                  color="transparent"
+                  className="btn-xs float-left py-0"
+                  id="load-notifications-btn"
+                  style={{ height: "45px", maxHeight: "45px" }}
+                >
+                  <i className="fa fa-refresh" /> Recycle count
+                </div>
+              </Col>
+              <Col md={3} xs={12}>
+                <div
+                  color="transparent"
+                  className="btn-xs float-left py-0"
+                  id="load-notifications-btn"
+                  style={{ height: "45px", maxHeight: "45px" }}
+                >
+                  <i className="fa fa-users" /> Number partners in the slot
+                </div>
+              </Col>
+            </Row>
+          </footer>
         </Widget>
       </div>
     );
