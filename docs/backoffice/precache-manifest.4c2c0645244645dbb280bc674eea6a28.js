710e234d0a74fdefcdb2d9319eb1dcd0cf7b422d
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-11-16 14:45:45 -0500

    update run final

---
+++ docs/backoffice/precache-manifest.4c2c0645244645dbb280bc674eea6a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0088a37664222f3dd675549580c2adaa",
+    "url": "/backoffice/index.html"
+  },
+  {
+    "revision": "614c850debcb706d0db1",
+    "url": "/backoffice/static/js/2.e0e95334.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/backoffice/static/js/2.e0e95334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b2b5f8f9725a6cde9e",
+    "url": "/backoffice/static/js/main.e07313d8.chunk.js"
+  },
+  {
+    "revision": "85ce668c226816938d46",
+    "url": "/backoffice/static/js/runtime-main.e478e202.js"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "/backoffice/static/media/TronLinkLogo.d3a8f115.png"
+  }
+]);
