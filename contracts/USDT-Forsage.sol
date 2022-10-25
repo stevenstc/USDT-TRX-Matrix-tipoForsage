f6ba1e0a18dd5a2f0cce3aaae5350305a3d966ae
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-10-25 17:03:31 -0500

    Update USDT-Forsage.sol

--- contracts/USDT-Forsage.sol
+++ contracts/USDT-Forsage.sol
@@ -5,13 +5,9 @@ pragma solidity >=0.5.15;
 interface TRC20_Interface {
 
     function allowance(address _owner, address _spender) external view returns (uint256);
-
     function transferFrom(address _from, address _to, uint _value) external returns (bool);
-
     function transfer(address direccion, uint cantidad) external returns (bool);
-
     function balanceOf(address who) external view returns (uint256);
-    
     function decimals() external view returns (uint256);
 }
 
